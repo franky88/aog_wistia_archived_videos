1b63abdd605e371947c676575cadb47bd4107c3a
Franklin Ramos <ramosfp99@gmail.com>
2025-10-08 08:08:34 -0400

    update filters and url search

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  // baseURL: "https://aog-wistia-archived-videos.vercel.app/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://aog-wistia-archived-videos.vercel.app/api",
+  // baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
