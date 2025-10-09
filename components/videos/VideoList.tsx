d2d3068e76b709884b9a6d5ed0e72806f741eb3a
Franklin Ramos <ramosfp99@gmail.com>
2025-10-09 05:07:40 -0400

    update filters and url search

--- components/videos/VideoList.tsx
+++ components/videos/VideoList.tsx
@@ -62,7 +62,7 @@ const VideoList = () => {
     const pages: number[] = [];
     const maxVisible = 5;
     let start = Math.max(1, page - Math.floor(maxVisible / 2));
-    let end = Math.min(totalPages, start + maxVisible - 1);
+    const end = Math.min(totalPages, start + maxVisible - 1);
 
     if (end - start + 1 < maxVisible) {
       start = Math.max(1, end - maxVisible + 1);
